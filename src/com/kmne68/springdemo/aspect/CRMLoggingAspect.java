901b057d7d223b572001158602a1eb40f29cd1eb
kmne68 <kmne68@gmail.com>
2021-02-08 23:57:25 -0600

    WIP adding AOP-based logging

---
+++ src/com/kmne68/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,24 @@
+package com.kmne68.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	// set up logger
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	// set up pointcut declarations
+	
+	
+	// add @Before advice
+	
+	
+	// add @AfterReturning advice
+	
+}
